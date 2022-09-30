0e404f20ce812300b5ec76a806b2d38ea2af6135
Pei Wei, Lin <408440450@gms.tku.edu.tw>
2022-10-01 03:21:27 +0800

    增加註解

--- 20220930/main.py
+++ 20220930/main.py
@@ -1,12 +1,15 @@
 if __name__ == "__main__":
     
+    # 讀取一個文字檔，並將其內容轉成list資料型態
     f = open("day1.txt", "r")
     data = f.read().splitlines()
 
+    # 將list內的數字轉成int型態
     data_length = len(data)
     for i in range(data_length):
         data[i] = int(data[i])
 
+    # 當後面的數字比前面數字大，則counter++
     counter = 0
     for i in range(data_length-1):
         if(data[i+1]-data[i]) > 0:
