311566b798f5bf3343b7b6450d1b95ba0fed9593
Pei Wei, Lin <408440450@gms.tku.edu.tw>
2022-10-14 17:36:16 +0800

    Completing answer checking

--- 20221014/wordle.py
+++ 20221014/wordle.py
@@ -5,3 +5,12 @@ if __name__ == "__main__":
 
     #指定一個正確答案（Commit）(簡易)
     answer = "apple"
+
+    #判斷使用者輸入與解答（Commit）
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
